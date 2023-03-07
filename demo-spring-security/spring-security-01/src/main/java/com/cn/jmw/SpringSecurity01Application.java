0be0938ea7bf72b88b785947a7b5ec85aa478984
王佳 <792836909@qq.com>
2023-03-07 16:48:54 +0800

    spring boot : SpringSecurity01 demo

---
+++ demo-spring-security/spring-security-01/src/main/java/com/cn/jmw/SpringSecurity01Application.java
@@ -0,0 +1,13 @@
+package com.cn.jmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity01Application.class, args);
+    }
+
+}
---
+++ demo-spring-security/spring-security-01/src/main/java/com/cn/jmw/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.cn.jmw.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("hello security");
+        return "hello spring security";
+    }
+    
+}
