692c4f126f6e80afeda9d569bcf22b1fa4b0a1c8
王佳 <792836909@qq.com>
2023-03-09 17:33:03 +0800

    spring boot : SpringAOP demo

---
+++ demo-spring/spring-aop/src/main/java/com/cn/jmw/SpringAOPApplication.java
@@ -0,0 +1,13 @@
+package com.cn.jmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAOPApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAOPApplication.class, args);
+    }
+
+}
