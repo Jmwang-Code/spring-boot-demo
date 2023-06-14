802396a8afc1a93bf491d78c3e360c0d5365bfae
王佳 <792836909@qq.com>
2023-06-14 18:43:05 +0800

    spring boot : Hadoop HDFS YARN

---
+++ demo-hadoop/src/main/java/com/cn/jmw/mapreduce/outputformat/LogOutputFormat.java
@@ -0,0 +1,19 @@
+package com.cn.jmw.mapreduce.outputformat;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
+public class LogOutputFormat extends FileOutputFormat<Text, NullWritable> {
+    @Override
+    public RecordWriter<Text, NullWritable> getRecordWriter(TaskAttemptContext job) throws IOException, InterruptedException {
+
+        LogRecordWriter lrw = new LogRecordWriter(job);
+
+        return lrw;
+    }
+}
