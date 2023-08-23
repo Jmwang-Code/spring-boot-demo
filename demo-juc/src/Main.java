94316c6592c976ee83c3023ece1de6a054993be8
wjm <792836909@qq.com>
2023-08-23 15:30:56 +0800

    JDK 代理DEMO

---
+++ demo-juc/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        ScheduledThreadPoolExecutor scheduledThreadPoolExecutor = new ScheduledThreadPoolExecutor(5);
+    }
+}
