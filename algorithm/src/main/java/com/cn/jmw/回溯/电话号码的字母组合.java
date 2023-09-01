07b451d439a2407cf67de5cb2c1e865248fdce9e
wjm <792836909@qq.com>
2023-09-01 18:46:54 +0800

    JDK 代理DEMO

---
+++ algorithm/src/main/java/com/cn/jmw/回溯/电话号码的字母组合.java
@@ -0,0 +1,48 @@
+package com.cn.jmw.回溯;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 电话号码的字母组合 {
+
+    public List<String> letterCombinations(String digits) {
+        List<String> combinations = new ArrayList<String>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        Map<Character, String> phoneMap = new HashMap<Character, String>() {{
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
+        }};
+        backtrack(combinations, phoneMap, digits, 0, new StringBuffer());
+        return combinations;
+    }
+
+    public void backtrack(List<String> combinations, Map<Character, String> phoneMap, String digits, int index, StringBuffer combination) {
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+        } else {
+            char digit = digits.charAt(index);
+            String letters = phoneMap.get(digit);
+            int lettersCount = letters.length();
+            for (int i = 0; i < lettersCount; i++) {
+                combination.append(letters.charAt(i));
+                backtrack(combinations, phoneMap, digits, index + 1, combination);
+                combination.deleteCharAt(index);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        电话号码的字母组合 电话号码的字母组合 = new 电话号码的字母组合();
+        电话号码的字母组合.letterCombinations("7");
+    }
+}
