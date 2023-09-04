873b35c8556ee5004c6bff1c6e9b1dcd8d70ca3e
wjm <792836909@qq.com>
2023-09-04 15:16:03 +0800

    JDK 代理DEMO

---
+++ algorithm/src/main/java/com/cn/jmw/递归算法/动态规划/背包问题.java
@@ -0,0 +1,11 @@
+package com.cn.jmw.递归算法.动态规划;
+
+
+/**
+ * 给定两个长度都为N的数组weights和values,
+ * weights[i]和values[i]分别代表i号物品的重量和价值。给定一个正数bag，表示一个载重bag的袋子，
+ * 你装的物品不能超过这个重量。
+ * 返回你能装下最多的价值是多少?
+ */
+public class 背包问题 {
+}
