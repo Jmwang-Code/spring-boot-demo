47c6f8812fe9fd132ea42dc597d73647c874fc21
wjm <792836909@qq.com>
2023-09-11 18:23:50 +0800

    MYSQL

---
+++ algorithm/src/main/java/allAlgorithm/modelABase/A算法和数据结构.java
@@ -0,0 +1,7 @@
+package allAlgorithm.modelABase;
+
+public class A算法和数据结构 {
+
+    //1. 常数时间的操作 ： + - * / %  >> >>> << | & ^ 数组寻址
+
+}
--- algorithm/src/main/java/com/cn/jmw/NikiniuInterview/algorithm.java
+++ algorithm/src/main/java/com/cn/jmw/NikiniuInterview/algorithm.java
@@ -1,18 +1,12 @@
 package com.cn.jmw.NikiniuInterview;
 
+import com.cn.jmw.递归算法.动态规划.机器人寻路;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class algorithm {
 
-    public static double calculate_it()
-    {
-        double x = 0.0d;
-        double y = 0.0d;
-        int total = 0;
-        for(int i = 0;i<7000000;i++){
-            x = Math.random();
-            y = Math.random();
-            if(Math.sqrt(x*x+y*y) < 1)
-                total++;
-        }
-        return total/1000000.0;
-    }
+
 }
