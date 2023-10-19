070e4b6b2418d205116152c1738513427bd0fdc2
wjm <792836909@qq.com>
2023-10-19 17:31:39 +0800

    algorithm

--- algorithm/src/main/java/allAlgorithm/BmodelLinkedList/NodeS.java
+++ algorithm/src/main/java/allAlgorithm/BmodelLinkedList/NodeS.java
@@ -1,5 +1,8 @@
 package allAlgorithm.BmodelLinkedList;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NodeS {
 
     public int val;
@@ -18,9 +21,9 @@ public class NodeS {
 
 /**
  * 输入 ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
- * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * [     [2],     [1, 1], [2, 2], [1],    [3, 3], [2], [4, 4], [1],   [3],   [4]]
  *
- * 输出 [null, null, null, 1, null, -1, null, -1, 3, 4]
+ * 输出 [null,      null,    null,    1,    null,  -1,   null,   -1,    3,     4]
  *
  * 解释
  * LRUCache lRUCache = new LRUCache(2);
@@ -34,16 +37,88 @@ public class NodeS {
  * lRUCache.get(4); // 返回 4
  */
 class LRUCache {
+    //用到的数据结构
+    class LinkedNode {
+        int key;
+        int value;
+        LinkedNode pre;
+        LinkedNode next;
+        public LinkedNode(){};
+        public LinkedNode(int newKey,int newValue) {
+            key = newKey;
+            value = newValue;
+        }
+    }
+
+    private Map<Integer,LinkedNode> cache = new HashMap<Integer,LinkedNode>();
+
+    private int size;
+    private int capacity;
+    private LinkedNode head;
+    private LinkedNode tail;
 
     public LRUCache(int capacity) {
-
+        this.size = 0;
+        this.capacity = capacity;
+        head = new LinkedNode();
+        tail = new LinkedNode();
+        head.next = tail;
+        tail.pre = head;
     }
 
     public int get(int key) {
+        LinkedNode node = cache.get(key);
+        if(node == null) {
+            return -1;
+        }
+
+        moveToHead(node);
+        return node.value;
+
 
     }
 
     public void put(int key, int value) {
+        LinkedNode node = cache.get(key);
+        if(node == null) {
+            LinkedNode newNode = new LinkedNode(key,value);
+            cache.put(key,newNode);
+            addToHead(newNode);
+            size++;
+            if(size > capacity) {
+                LinkedNode tail = removeTail();
+                cache.remove(tail.key);
+                size--;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
 
+
+
+    private void addToHead(LinkedNode node) {
+        node.pre = head;
+        node.next = head.next;
+        head.next.pre = node;
+        head.next = node;
+    }
+
+    private LinkedNode removeTail() {
+        LinkedNode res = tail.pre;
+        removeNode(res);
+        return res;
+    }
+
+    private void removeNode(LinkedNode node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+
+    }
+
+    private void moveToHead(LinkedNode node) {
+        removeNode(node);
+        addToHead(node);
     }
 }
