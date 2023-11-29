06337408f1a03b730b389ca5f099f0f227945478
wjm <792836909@qq.com>
2023-11-29 22:39:05 +0800

    CommonEnum

--- demo-activiti7/src/main/java/com/cn/jmw/activiti/common/CommonEnum.java
+++ demo-activiti7/src/main/java/com/cn/jmw/activiti/common/CommonEnum.java
@@ -5,7 +5,11 @@ public enum CommonEnum {
     /**
      * 流程状态
      */
-    ASSIGNEE("T1_Assignee");
+    T1_ASSIGNEE("T1_Assignee"),
+    T2_ASSIGNEE("T2_Assignee"),
+    T3_ASSIGNEE("T3_Assignee"),
+    T4_ASSIGNEE("T4_Assignee"),
+    T5_ASSIGNEE("T5_Assignee");
 
     private String code;
 
