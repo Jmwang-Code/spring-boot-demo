390b0aee9707900d85724942b524de00746f11de
wjm <792836909@qq.com>
2023-12-04 19:39:19 +0800

    JDK19 OKHTTP

---
+++ demo-okhttp/src/main/java/com/cn/jmw/SpringBootOkhttpApplication.java
@@ -0,0 +1,14 @@
+package com.cn.jmw;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOkhttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOkhttpApplication.class, args);
+    }
+
+}
