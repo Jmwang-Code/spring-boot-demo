c855b4832efc051e169583d81399189983f02317
wjm <792836909@qq.com>
2024-03-02 10:44:29 +0800

    unix知识demo

---
+++ unix/src/main/java/com/cn/jmw/UnixApplication.java
@@ -0,0 +1,12 @@
+package com.cn.jmw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnixApplication.class, args);
+    }
+}
