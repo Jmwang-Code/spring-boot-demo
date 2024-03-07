e8082f01db04a95e2011fa8633285b46e88196ac
wjm <792836909@qq.com>
2024-03-07 12:29:15 +0800

    01背包

--- algorithm/src/main/java/com/cn/jmw/递归算法/动态规划/背包问题/背包问题简单20240307二周目.java
+++ algorithm/src/main/java/com/cn/jmw/递归算法/动态规划/背包问题/背包问题简单20240307二周目.java
@@ -31,11 +31,39 @@ public class 背包问题简单20240307二周目 {
         return Math.max(ans1,ans2);
     }
 
+    /**
+     * 动态规划
+     *
+     * 跳过dp缓存的方法二
+     */
+    public static int maxValue2(int[] w, int[] v, int bag) {
+        int N = w.length;
+        //index 0 - N
+        // bag 0 - bag
+        int[][] dp = new int[N+1][bag+1];
+        //n行所有位置都是0
+
+        for (int index = N-1; index >= 0; index--) {
+            for (int bagRest = 0; bagRest <= bag; bagRest++) {
+                int p1 = dp[index+1][bagRest];
+                int p2 = -1;
+                if (bagRest - w[index] >= 0){
+                    p2 = v[index]+dp[index+1][bagRest-w[index]];
+                }
+                dp[index][bagRest] = Math.max(p1,p2);
+            }
+        }
+
+        return dp[0][bag];
+    }
+
     public static void main(String[] args) {
         //测试
         int[] w = {2, 1, 3, 4};
         int[] v = {5, 6, 3, 19};
         int bag = 8;
         System.out.println(maxValue1(w, v, bag));
+
+        System.out.println(maxValue2(w, v, bag));
     }
 }
