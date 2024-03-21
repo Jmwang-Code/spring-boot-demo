ee220952e7d4b6f512617349ae40c7a413ac30a4
wjm <792836909@qq.com>
2024-03-21 16:46:08 +0800

    ლ(′◉❥◉｀ლ)

--- My-Resume/JAVA-BASE-CONLLECTIONS/src/main/java/MyCollections.java
+++ My-Resume/JAVA-BASE-CONLLECTIONS/src/main/java/MyCollections.java
@@ -6,6 +6,14 @@ public class MyCollections {
 
     @Test
     public void Java集合框架有那些接口(){
-        MyArrayList<Object> objects = new MyArrayList();
+        MyArrayList<String> list = new MyArrayList();
+
+        list.add("0");
+        list.add("1");
+        list.add("2");
+
+        System.out.println(list.get(1));
+        System.out.println(list.size());
+        System.out.println(list.remove(1));
     }
 }
