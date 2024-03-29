53eca97ec1a627bf12a6019a4a4072542f5a09b6
wjm <792836909@qq.com>
2024-03-29 23:42:03 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/递归算法/动态规划/LCR090打家劫舍II.java
+++ algorithm/src/main/java/com/cn/jmw/递归算法/动态规划/LCR090打家劫舍II.java
@@ -72,54 +72,33 @@ public class LCR090打家劫舍II {
      * 动态规划 TODO 有问题
      */
     public int rob3(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        if (nums.length == 1) {
+        if(nums.length==1)
             return nums[0];
-        }
-
-        if (nums.length == 2){
+        if(nums.length == 2)
             return Math.max(nums[0],nums[1]);
+        return Math.max(priority(Arrays.copyOfRange(nums,0,nums.length-1)),
+                priority(Arrays.copyOfRange(nums,1,nums.length)));
+    }
+    private int priority(int [] nums){
+        int n=nums.length;
+        int [] dp=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1]);
         }
-
-        int[][] dp = new int[nums.length + 1][nums.length + 1];
-        //当end为nums.length
-        dp[nums.length][nums.length] = 0;
-        dp[nums.length-1][nums.length] = nums[nums.length - 1];
-        dp[nums.length-2][nums.length] = nums[nums.length - 2];
-        //当end为nums.length-1
-        dp[nums.length-1][nums.length-1] = nums[nums.length - 1];
-        dp[nums.length-2][nums.length-1] = nums[nums.length - 2];
-        dp[nums.length-3][nums.length-1] = nums[nums.length - 3];
-
-        //计算的时候分为 end 范围为 [0,nums.length-1] 和 [1,nums.length] 两种情况
-        for (int i = nums.length-3; i > 0; i--) {
-            dp[i][nums.length] = Math.max(nums[i] + dp[i + 2][nums.length],nums[i] + dp[i + 3][nums.length]);
-        }
-
-        for (int i = nums.length-4; i >= 0; i--) {
-            if (nums.length <= 4){
-                dp[i][nums.length-1] = Math.max(nums[i] + dp[i + 2][nums.length-1],nums[i]);
-            }else {
-                dp[i][nums.length-1] = Math.max(nums[i] + dp[i + 2][nums.length-1],nums[i] + dp[i + 3][nums.length-1]);
-            }
-        }
-
-
-        return Math.max(dp[0][nums.length-1],Math.max(dp[1][nums.length],dp[2][nums.length]));
+        return dp[n-1];
     }
 
 
     @Test
     public void test() {
-//        System.out.println(rob3(new int[]{2, 3, 2}));
-//        System.out.println(rob3(new int[]{1, 2, 3, 1}));
-//        System.out.println(rob3(new int[]{0}));
-//        System.out.println(rob3(new int[]{226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124}));
-//        System.out.println(rob3(new int[]{6,6,4,8,4,3,3,10}));
-//        System.out.println(rob3(new int[]{2,1,1,2}));
+        System.out.println(rob3(new int[]{2, 3, 2}));
+        System.out.println(rob3(new int[]{1, 2, 3, 1}));
+        System.out.println(rob3(new int[]{0}));
+        System.out.println(rob3(new int[]{226,174,214,16,218,48,153,131,128,17,157,142,88,43,37,157,43,221,191,68,206,23,225,82,54,118,111,46,80,49,245,63,25,194,72,80,143,55,209,18,55,122,65,66,177,101,63,201,172,130,103,225,142,46,86,185,62,138,212,192,125,77,223,188,99,228,90,25,193,211,84,239,119,234,85,83,123,120,131,203,219,10,82,35,120,180,249,106,37,169,225,54,103,55,166,124}));
+        System.out.println(rob3(new int[]{6,6,4,8,4,3,3,10}));
+        System.out.println(rob3(new int[]{2,1,1,2}));
         System.out.println(rob3(new int[]{2,2,4,3,2,5}));
     }
 }
