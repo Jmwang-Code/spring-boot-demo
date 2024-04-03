998b87b1c6e24116d75f9b6cb44843f9cfc8bb24
wjm <792836909@qq.com>
2024-04-03 17:29:27 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/键键盘5.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/键键盘5.java
@@ -1,9 +1,14 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 /**
+ *╭—————————————————————————————╮
+ *│ 当前时间：2024-04-03 16:30:14 │
+ *│ 完成时间：00:20:02            │
+ *╰—————————————————————————————╯
  * 题目描述
  * 有一个特殊的5键键盘，上面有a，ctrl-c，ctrl-x，ctrl-v，ctrl-a五个键。
  *
@@ -32,12 +37,52 @@ import java.util.Scanner;
  */
 public class 键键盘5 {
     public static void main(String[] args) {
+        //帮我举出一堆测试案例 最好极端
+        //比如 1 1 5 1 5 2 4 4
         Scanner sc = new Scanner(System.in);
         int[] commands = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         System.out.println(getResult(commands));
     }
 
     public static int getResult(int[] commands) {
-        return 0;
+        // 用于存储屏幕上的字符
+        ArrayList<String> screen = new ArrayList<>();
+        // 用于存储剪贴板上的字符
+        ArrayList<String> clip = new ArrayList<>();
+        boolean isSelect = false;
+
+        for (int command : commands) {
+            switch (command) {
+                case 1: // a
+                    if (isSelect) screen.clear();
+                    screen.add("a");
+                    isSelect = false;
+                    break;
+                case 2: // ctrl-c
+                    if (isSelect) {
+                        clip.clear();
+                        clip.addAll(screen);
+                    }
+                    break;
+                case 3: // ctrl-x
+                    if (isSelect) {
+                        clip.clear();
+                        clip.addAll(screen);
+                        screen.clear();
+                        isSelect = false;
+                    }
+                    break;
+                case 4: // ctrl-v
+                    if (isSelect) screen.clear();
+                    screen.addAll(clip);
+                    isSelect = false;
+                    break;
+                case 5: // ctrl-a
+                    if (screen.size() != 0) isSelect = true;
+                    break;
+            }
+        }
+
+        return screen.size();
     }
 }
