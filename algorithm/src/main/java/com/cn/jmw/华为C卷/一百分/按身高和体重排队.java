817639e2bfd81ccce62e03a558d0278b4337027b
wjm <792836909@qq.com>
2024-04-08 12:13:56 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/按身高和体重排队.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/按身高和体重排队.java
@@ -1,4 +1,10 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Scanner;
+
 public class 按身高和体重排队 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
 }
--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/爱吃蟠桃的孙悟空.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/爱吃蟠桃的孙悟空.java
@@ -1,4 +1,40 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *╭—————————————————————————————╮
+ *│ 当前时间：2024-04-08 12:13:06 │
+ *│ 完成时间：00:14:21            │
+ *╰—————————————————————————————╯
+ *
+ */
 public class 爱吃蟠桃的孙悟空 {
+
+    //有N棵树
+    //每小时吃K个
+    //H小时后回来
+    //每次选一棵树吃，吃k个，如果小于K就全吃完。
+
+    //返回K，也就是用最小速度吃完。如果多大速度都吃不完，就返回0。
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //N个桃树，并且每个桃树的数量
+        while (scanner.hasNext()){
+            String line = scanner.nextLine();
+            if (line.isEmpty()) // 如果输入行为空，则继续下一次循环
+                continue;
+            int[] N = Arrays.stream(line.split(" ")).mapToInt(Integer::parseInt).toArray();
+            int H = scanner.nextInt();
+            if (H<N.length){
+                System.out.println(0);
+            }else {
+                //余
+                int mod = H%N.length;
+                Arrays.sort(N);
+                System.out.println(N[N.length-1-mod]);
+            }
+        }
+    }
 }
