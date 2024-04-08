d392f9e78f806c7661abb017ae63bf60ac97e01a
wjm <792836909@qq.com>
2024-04-08 14:22:17 +0800

    ლ(′◉❥◉｀ლ)

---
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/Main.java
@@ -0,0 +1,93 @@
+package com.cn.jmw.华为C卷.一百分;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(getResult(sc.nextInt()));
+    }
+
+    public static int getResult(int n) {
+        CycleLinkedList list = new CycleLinkedList();
+        for (int i = 1; i <= n; i++) list.append(i);
+
+        int num = 1;
+        Node cur = list.head;
+
+        while (list.size > 1) {
+            if (num == 3) {
+                num = 1;
+                cur = list.remove(cur);
+            } else {
+                num++;
+                cur = cur.next;
+            }
+        }
+
+        return cur.val;
+    }
+}
+
+class Node {
+    int val;
+    Node prev;
+    Node next;
+
+    public Node(int val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null;
+    }
+}
+
+class CycleLinkedList {
+    Node head;
+    Node tail;
+    int size;
+
+    public CycleLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    public void append(int val) {
+        Node node = new Node(val);
+
+        if (this.size > 0) {
+            this.tail.next = node;
+            node.prev = this.tail;
+        } else {
+            this.head = node;
+        }
+
+        this.tail = node;
+
+        this.head.prev = this.tail;
+        this.tail.next = this.head;
+        this.size++;
+    }
+
+    public Node remove(Node cur) {
+        Node pre = cur.prev;
+        Node nxt = cur.next;
+
+        pre.next = nxt;
+        nxt.prev = pre;
+
+        cur.next = cur.prev = null;
+
+        if (this.head == cur) {
+            this.head = nxt;
+        }
+
+        if (this.tail == cur) {
+            this.tail = pre;
+        }
+
+        this.size--;
+
+        return nxt;
+    }
+}
--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/报数问题.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/报数问题.java
@@ -1,4 +1,57 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Scanner;
+
+/**
+ *╭—————————————————————————————╮
+ *│ 当前时间：2024-04-08 14:21:57 │
+ *│ 完成时间：00:30:54            │
+ *╰—————————————————————————————╯
+ * 
+ */
 public class 报数问题 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()){
+            int hua = scanner.nextInt();
+            Node node = new Node(1);
+            Node temp = node;
+            for (int i = 2; i <= hua; i++) {
+                temp.next = new Node(i);
+                temp = temp.next;
+            }
+
+            temp.next = node;
+
+            int size = 1;
+            Node pre = node;
+            Node cur = node;
+            while (pre.next.var!= pre.var){
+                if (size==3){
+                    pre.next = cur.next;
+                    size=1;
+                    cur = cur.next;
+                    continue;
+                }
+                pre = cur;
+                cur = cur.next;
+                size++;
+            }
+
+            System.out.println(cur.var);
+        }
+    }
+
+    static class Node{
+
+        int var;
+        Node next;
+
+        public Node(int var){
+            this.var = var;
+        }
+
+    }
 }
