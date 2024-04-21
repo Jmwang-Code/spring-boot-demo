528c06e2710a85f43ab26623d0f3d38816cdfda9
wjm <792836909@qq.com>
2024-04-21 21:49:00 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/滑动窗口最大和.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/滑动窗口最大和.java
@@ -1,18 +1,40 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Scanner;
+
 public class 滑动窗口最大和 {
 
-    
+    // 输入获取
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
 
-    public static String getResult() {
-	// 在这里编写你自定义的方法逻辑
-        return null;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int m = sc.nextInt();
+
+        System.out.println(getResult(n, arr, m));
     }
 
+    // 算法入口
+    public static int getResult(int n, int[] arr, int m) {
+        int sum = 0;
+        for (int i = 0; i < m; i++) { // 初始滑窗内部和
+            sum += arr[i];
+        }
 
-    public static void main(String[] args) {
+        int ans = sum;
 
-   }
+        for (int i = 1; i <= n - m; i++) {
+            sum += arr[i + m - 1] - arr[i - 1]; // 基于初始滑窗进行差异求和，避免O(n)求和
+            ans = Math.max(ans, sum);
+        }
+
+        return ans;
+    }
 
 }
