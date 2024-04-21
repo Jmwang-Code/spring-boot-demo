4b83ce94ef243925a466df40f7694d83726fe5c0
wjm <792836909@qq.com>
2024-04-22 01:15:12 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/火星文计算.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/火星文计算.java
@@ -1,18 +1,22 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Scanner;
+
 public class 火星文计算 {
 
     
 
 
-    public static String getResult() {
+    public static long getResult(String str) {
 	// 在这里编写你自定义的方法逻辑
-        return null;
+        return -1;
     }
 
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(getResult(str));
    }
 
 }
