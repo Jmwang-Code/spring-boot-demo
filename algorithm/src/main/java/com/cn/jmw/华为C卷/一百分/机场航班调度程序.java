97828f0cfdc14134a7c559cb12c3a6036d2b7dd4
wjm <792836909@qq.com>
2024-04-22 03:33:30 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/机场航班调度程序.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/机场航班调度程序.java
@@ -1,18 +1,35 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class 机场航班调度程序 {
 
-    
-
-
-    public static String getResult() {
-	// 在这里编写你自定义的方法逻辑
-        return null;
-    }
-
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-   }
+        String[] flights = sc.nextLine().split(",");
+
+        StringJoiner sj = new StringJoiner(",");
+        Arrays.stream(flights)
+                .sorted(
+                        (a, b) -> {
+                            String abbr1 = a.substring(0, 2);
+                            String num1 = a.substring(2);
+
+                            String abbr2 = b.substring(0, 2);
+                            String num2 = b.substring(2);
+
+                            if (abbr1.equals(abbr2)) {
+                                return num1.compareTo(num2);
+                            } else {
+                                return abbr1.compareTo(abbr2);
+                            }
+                        })
+                .forEach(sj::add);
+
+        System.out.println(sj);
+    }
 
 }
