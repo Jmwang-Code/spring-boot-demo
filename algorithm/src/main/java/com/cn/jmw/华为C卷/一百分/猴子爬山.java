0e62b788dcccb00ac87c0a0dd5daddac2a9d5671
wjm <792836909@qq.com>
2024-04-21 21:41:51 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/猴子爬山.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/猴子爬山.java
@@ -1,18 +1,29 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Scanner;
+
 public class 猴子爬山 {
 
-    
+    public static int getResult(int target,int indexCur) {
+        if (indexCur>target){
+            return 0;
+        }
 
+        if (indexCur==target){
+            return 1;
+        }
 
-    public static String getResult() {
-	// 在这里编写你自定义的方法逻辑
-        return null;
+        int sum = 0;
+        sum += getResult(target,indexCur+1);
+        sum += getResult(target,indexCur+3);
+        return sum;
     }
 
 
     public static void main(String[] args) {
-
-   }
+        Scanner scanner = new Scanner(System.in);
+        int target = scanner.nextInt();
+        System.out.println(getResult(target,0));
+    }
 
 }
