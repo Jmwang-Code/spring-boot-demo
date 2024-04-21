27235586bbc59213a05c8549e32c22d4318650fa
wjm <792836909@qq.com>
2024-04-21 22:39:02 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/绘图机器.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/绘图机器.java
@@ -1,18 +1,40 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Scanner;
+
 public class 绘图机器 {
 
-    
-
-
-    public static String getResult() {
-	// 在这里编写你自定义的方法逻辑
-        return null;
-    }
-
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-   }
+        int n = sc.nextInt();
+        int end_x = sc.nextInt();
+
+        // 记录题解
+        long ans = 0;
+
+        long last_x = 0; // 上一个点的横坐标
+        long last_y = 0; // 上一个点的纵坐标
+
+        // 获取n行输入
+        for (int i = 0; i < n; i++) {
+            int cur_x = sc.nextInt(); // 当前点的横坐标
+            int offset_y = sc.nextInt(); // 当前点纵坐标相较于上一个点纵坐标的偏移量
+
+            // cur_x - last_x 结果是上一个点到当前点的横向距离, 这个距离过程中，高度保持为abs(last_y)
+            ans += (cur_x - last_x) * Math.abs(last_y);
+
+            // 更新last_x, last_y
+            last_x = cur_x;
+            last_y += offset_y;
+        }
+
+        // 注意结束位置的处理
+        if (end_x > last_x) {
+            ans += (end_x - last_x) * Math.abs(last_y);
+        }
+
+        System.out.println(ans);
+    }
 
 }
