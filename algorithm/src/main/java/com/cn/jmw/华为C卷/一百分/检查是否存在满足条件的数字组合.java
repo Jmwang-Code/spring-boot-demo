d5d6e39abf2bae92988fc0b3cf5c08d7b82a5d87
wjm <792836909@qq.com>
2024-04-23 20:00:17 +0800

    ლ(′◉❥◉｀ლ)

--- algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/检查是否存在满足条件的数字组合.java
+++ algorithm/src/main/java/com/cn/jmw/华为C卷/一百分/检查是否存在满足条件的数字组合.java
@@ -1,18 +1,64 @@
 package com.cn.jmw.华为C卷.一百分;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class 检查是否存在满足条件的数字组合 {
 
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = Integer.parseInt(sc.nextLine());
+        Integer[] arr =
+                Arrays.stream(sc.nextLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
 
-    public static String getResult() {
-	// 在这里编写你自定义的方法逻辑
-        return null;
+        System.out.println(getResult(n, arr));
     }
 
+    public static String getResult2(int n, Integer[] arr) {
+        Arrays.sort(arr, (a, b) -> b - a);
 
-    public static void main(String[] args) {
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] == arr[j] + 2 * arr[k]) {
+                        return arr[i] + " " + arr[j] + " " + arr[k];
+                    }
+                    if (arr[i] == arr[k] + 2 * arr[j]) {
+                        return arr[i] + " " + arr[k] + " " + arr[j];
+                    }
+                }
+            }
+        }
 
-   }
+        return "0";
+    }
+
+    public static String getResult(int n, Integer[] arr) {
+        Arrays.sort(arr, (a, b) -> a - b);
+
+        for (int i = arr.length-1; i > 1; i--) {
+            int right = i, middle = 1, left = 0;
+            int target =  arr[right];
+            while (left < middle && middle < right) {
+                if (target > arr[middle] + arr[left] * 2) {
+                    //是否一起移动
+                    if (target > arr[middle+1] + arr[left] * 2){
+                        middle++;
+                        left++;
+                    }else {
+                        //移动mid
+                        middle++;
+                    }
+                } else if (target < arr[middle] + arr[left] * 2) {
+                    break;
+                } else {
+                    return arr[right] + " " + arr[middle] + " " + arr[left];
+                }
+            }
+        }
+
+        return "0";
+    }
 
 }
