0e5aa2df4e53ed372c3a97c71b70c34e4e5dca37
wjm <792836909@qq.com>
2024-04-24 12:44:03 +0800

    ლ(′◉❥◉｀ლ)

---
+++ demo-source-code/src/main/java/SegmentTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
+public class SegmentTest {
+
+    @Test
+    public void ConcurrentHashMap_Segment(){
+        Map map = new ConcurrentHashMap();
+        map.put(1,1);
+    }
+}
