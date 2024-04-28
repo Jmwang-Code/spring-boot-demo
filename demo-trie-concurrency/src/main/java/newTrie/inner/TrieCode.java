59552733317ea9c8dcc391996c8fcd115853b5de
wjm <792836909@qq.com>
2024-04-28 22:40:48 +0800

    构建新字典树结构

--- demo-trie-concurrency/src/main/java/newTrie/inner/TrieCode.java
+++ demo-trie-concurrency/src/main/java/newTrie/inner/TrieCode.java
@@ -14,6 +14,19 @@ public class TrieCode implements Serializable {
     private int code;
     private byte type;
 
+    public TrieCode() {
+    }
+
+    public TrieCode(int code, byte type) {
+        this.code = code;
+        this.type = type;
+    }
+
+    public TrieCode(int code, int type) {
+        this.code = code;
+        this.type = (byte) type;
+    }
+
     public int getCode() {
         return code;
     }
@@ -28,5 +41,35 @@ public class TrieCode implements Serializable {
 
     public void setType(byte type) {
         this.type = type;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + code;
+        result = prime * result + type;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        TrieCode other = (TrieCode) obj;
+        if (code != other.code)
+            return false;
+        if (type != other.type)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "TrieCode [code=" + code + ", type=" + type + "]";
     }
 }
