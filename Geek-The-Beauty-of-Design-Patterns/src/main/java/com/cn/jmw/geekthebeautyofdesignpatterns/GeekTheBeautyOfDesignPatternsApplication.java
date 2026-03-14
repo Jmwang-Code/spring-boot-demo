d723ae67d9feb50f6a8bf8fcd32a34d4c9f4d1c8
79283 <792836909@qq.com>
2026-03-14 16:59:02 +0800

    极客-设计模式之美-1

---
+++ Geek-The-Beauty-of-Design-Patterns/src/main/java/com/cn/jmw/geekthebeautyofdesignpatterns/GeekTheBeautyOfDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.cn.jmw.geekthebeautyofdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekTheBeautyOfDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekTheBeautyOfDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ Geek-The-Beauty-of-Design-Patterns/src/test/java/com/cn/jmw/geekthebeautyofdesignpatterns/GeekTheBeautyOfDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.jmw.geekthebeautyofdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekTheBeautyOfDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
